e2697590187738ff0aa45a56c5d75c4c55ac8337
Giacomo Melacini <gmelacini@unibz.it>
2022-01-26 16:19:45 +0100

    Added max depth in alphabeta

--- alphabeta.pl
+++ alphabeta.pl
@@ -1,14 +1,19 @@
 % alpha beta relation. GoodPos is a possible next position that satisfies the alpha beta values.
 % first finds the possible moves from Pos, finds a good enough position if Pos is not a leaf
 % otherwise it takes the value of the leaf node
-alphabeta(Pos, Alpha, Beta, GoodPos, Val) :-
+alphabeta(Pos, Alpha, Beta, GoodPos, Val, Depth, MaxDepth) :-
   moves(Pos, PosList), !,
-  (boundedbest(PosList, Alpha, Beta, GoodPos, Val) ; staticval(Pos, Val).
+  Depth < MaxDepth,
+  boundedbest(PosList, Alpha, Beta, GoodPos, Val, Depth, MaxDepth).
+
+alphabeta(Pos, Alpha, Beta, GoodPos, Val, _, _) :-
+  staticval(Pos, Val).
 
 % finds a good enough position GoodPos in the list Poslist so that the backed-up
 % value Val of GoodPos is a good enough approximation with respect to Alpha and Beta
-boundedbest([Pos|PosList], Alpha, Beta, GoodPos, GoodVal) :-
-  alphabeta(Pos, Alpha, Beta, _, Val),
+boundedbest([Pos|PosList], Alpha, Beta, GoodPos, GoodVal, Depth, MaxDepth) :-
+  NewDepth is Depth + 1,
+  alphabeta(Pos, Alpha, Beta, _, Val, NewDepth, MaxDepth),
   goodenough(Poslist, Alpha, Beta, Pos, Val, GoodPos, GoodVal).
 
 % no other candidates
